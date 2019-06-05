77bf6b35a161b624a01cc19baccdef00630dae11
Renan Garcia <renan.garcia@fretebras.com.br>
2019-06-05 15:28:32 -0300

    First version renangarcia resume

---
+++ gatsby-config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+
+module.exports = {
+  siteMetadata: {
+    title: 'Renan Garcia',
+    author: `@renangarciasoz`,
+    description:
+      'Minha missão é criar interfaces incríveis e de alta performance provendo uma ótima experiência para o usuário',
+    siteUrl: 'https://renangarcia.com', // full path to blog - no ending slash
+  },
+  mapping: {
+    'MarkdownRemark.frontmatter.author': 'AuthorYaml',
+  },
+  plugins: [
+    `gatsby-plugin-offline`,
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: path.join(__dirname, 'src', 'content'),
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem',
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-abbr',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1170,
+              quality: 90,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://renangarcia.com',
+      },
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-feed',
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-136788761-2',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // IP anonymization for GDPR compliance
+        anonymize: true,
+        // Disable analytics for users with `Do Not Track` enabled
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+        // Specifies what percentage of users should be tracked
+        sampleRate: 100,
+        // Determines how often site speed tracking beacons will be sent
+        siteSpeedSampleRate: 10,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Renan Garcia`,
+        short_name: `rGarcia`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#000`,
+        display: `standalone`,
+        legacy: false,
+        include_favicon: false,
+        cache_busting_mode: `none`,
+        icon: `src/content/img/renan-icon.png`,
+      },
+    },
+  ],
+};
---
+++ src/website-config.ts
@@ -0,0 +1,69 @@
+export interface WebsiteConfig {
+  title: string;
+  description: string;
+  coverImage: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Meta tag for Google Webmaster Tools
+   */
+  googleSiteVerification?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+}
+
+const config: WebsiteConfig = {
+  author: 'Renan Garcia',
+  title: 'Renan Garcia Blog',
+  description: 'Um blog de um desenvolvedor Front End',
+  coverImage: 'img/blog-cover.jpg',
+  logo: 'img/renan-logo.png',
+  lang: 'pt-br',
+  siteUrl: 'https://renangarcia.com',
+  facebook: 'https://www.facebook.com/renan.garcia.927',
+  twitter: 'https://twitter.com/_reehgarcia',
+  showSubscribe: true,
+  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpEmailFieldName: 'MERGE0',
+  googleSiteVerification: 'GoogleCode',
+  footer: 'is Based on Gatsby',
+};
+
+export default config;
